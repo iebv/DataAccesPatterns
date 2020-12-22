034df1b68d33ac9bba44b7a0b2d5fbb8e96fd3b5
Ivan Ballesteros <ivan.ballesteros@csgi.com>
2020-12-21 19:14:55 -0500

    Repository pattern applied
    Unit of work pattern applied
    Lazy initialization pattern applied with IValueHolder

---
+++ MyShop.Infrastructure/Repositories/CustomerRepository.cs
@@ -0,0 +1,68 @@
+﻿using MyShop.Domain.Lazy;
+using MyShop.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace MyShop.Infrastructure.Repositories
+{
+    public class CustomerRepository : GenericRepository<Customer>
+    {
+        public CustomerRepository(ShoppingContext context) : base(context)
+        {
+        }
+
+        public override IEnumerable<Customer> All()
+        {
+            return base.All().Select(c =>
+            {
+                c.ProfilePictureValueHolder = new ValueHolder<byte[]>((parameter) =>
+                {
+                    return ProfilePictureService.GetFor(parameter.ToString());
+                });
+                return c;
+            });
+
+        }
+
+        public override IEnumerable<Customer> Find(Expression<Func<Customer, bool>> predicate)
+        {
+            return base.Find(predicate).Select(c =>
+            {
+                c.ProfilePictureValueHolder = new ValueHolder<byte[]>((parameter) =>
+                {
+                    return ProfilePictureService.GetFor(parameter.ToString());
+                });
+                return c; 
+            });
+        }
+
+        public override Customer Get(Guid id)
+        {
+            var customer = base.Get(id);
+            customer.ProfilePictureValueHolder = new ValueHolder<byte[]>((parameter) =>
+            {
+                return ProfilePictureService.GetFor(parameter.ToString());
+            });
+
+            return customer;
+        }
+
+        public override Customer Update(Customer entity)
+        {
+
+            var customer = context.Customers
+                .Single(c => c.CustomerId == entity.CustomerId);
+
+            customer.Name = entity.Name;
+            customer.City = entity.City;
+            customer.PostalCode = entity.PostalCode;
+            customer.ShippingAddress = entity.ShippingAddress;
+            customer.Country = entity.Country;
+
+            return base.Update(customer);     
+        }
+    }
+}
